51322f08922feb3bda3b86645f1b2da206d59adb
Piyush9519 <112774924+Piyush9519@users.noreply.github.com>
2025-08-28 12:18:33 +0530

    Create Write a program to declare and print an integer, a float, and a character variable.c

---
+++ Write a program to declare and print an integer, a float, and a character variable.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int age = 18;
+    float height = 5.7;
+    char grade = 'A';
+    printf("Integer value(Age): %d\n", age);
+    printf("Float value(Height): %.2f\n", height);
+    printf("Character value(Grade): %c\n", grade);
+    return 0;
+}
