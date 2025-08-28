3c860e29d1b66224627ecceb63ce10efb529dde2
Piyush9519 <112774924+Piyush9519@users.noreply.github.com>
2025-08-28 12:04:55 +0530

    Create Write a program to print your name, course, and university.c.c

---
+++ Write a program to print your name, course, and university.c.c
@@ -0,0 +1,8 @@
+ #include <stdio.h>
+ int main(){
+     printf("Name: Piyush Kushwaha\n");
+     printf("Course: Btech in Computer Science\n");
+     printf("University: Chandigarh University\n");
+
+     return 0;
+ }
