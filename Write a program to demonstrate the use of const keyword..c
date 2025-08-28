3a1569c8e0846e9c2dd6336b2c39c8aa5a122d36
Piyush9519 <112774924+Piyush9519@users.noreply.github.com>
2025-08-28 14:37:28 +0530

    Create Write a program to demonstrate the use of const keyword..c

---
+++ Write a program to demonstrate the use of const keyword..c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    const int Days_in_week = 7;
+    printf("Number of Days in a week: %d\n", Days_in_week);
+
+    const char *Name = "Piyush";
+    printf("Name: %s\n", Name);
+
+    return 0;
+}
